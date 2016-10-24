e0bdc09ef3ffb5f820ab41230b14dbb163e992ec
PhoenixFire13 <gaoj@bxscience.edu>
2016-10-24 09:55:01 -0400

    Add files via upload

---
+++ DelaySoundEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelaySoundEffect : MonoBehaviour {
+
+    AudioSource aud;
+
+    public float delayTime;
+
+	// Use this for initialization
+	void Start () {
+        aud = GetComponent<AudioSource>();
+        StartCoroutine("InitialWaitAndPlay");
+    }
+
+	// Update is called once per frame
+	IEnumerator InitialWaitAndPlay()
+    {
+        yield return new WaitForSeconds(delayTime);
+        aud.Play();
+
+        StartCoroutine("InitialWaitAndPlay");
+    }
+}

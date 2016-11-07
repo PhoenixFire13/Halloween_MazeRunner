328540e0e5d9be7fc11d08498255b394190c9209
InuKami16 <baor1@bxscience.edu>
2016-11-06 19:19:59 -0500

    Update

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine.UI;
 
@@ -8,13 +8,17 @@ public class ButtonManager : MonoBehaviour {
     private PageNumber page;
     private Button button;
     private Text txt;
-    public string[] pageText;
+    private string[] pageText;
 
 	// Use this for initialization
 	void Start () {
         page = gameObject.GetComponentInParent<PageNumber>();
         button = gameObject.GetComponent<Button>();
         txt = button.GetComponentInParent<Text>();
+        pageText = new string[3];
+        pageText[0] = "\n\n\nControls: \n\nUse the WASD or arroy keys to control the player and navagate the maze";
+        pageText[1] = "\n\nHeart Rate: \n\nYour character's heart rate is displayed at the bottom left hand corner.\nIf you're heart rate raises too high, you might die from heart attack.";
+        pageText[2] = "Objective: \n\nFind the key on the third floor to open the exit\n\nExtra Objective:\n\nGo to the third level of the basement to find a hidden treasure, but beware, the basement is full of hidden dangers";
         //Debug print(txt);
         txt.text = pageText[page.getPageNum()];
 	}
@@ -48,7 +52,8 @@ public class ButtonManager : MonoBehaviour {
     {
         if (page.getPageNum() == 0)
         {
-            EditorSceneManager.LoadScene("GameManager");
+            SceneManager.LoadScene("GameMenu");
+            return;
         }
 
         //button.gameObject.SetActive(true);

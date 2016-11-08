ef876e3a88d6bbaa5b5aec3e282536817e9e81ef
PhoenixFire13 <gaoj@bxscience.edu>
2016-11-08 12:51:22 -0500

    Add files via upload

---
+++ Assets/Scripts/TrapManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapManager : MazeCell {
+
+    ParticleSystem trap;
+
+    void Awake()
+    {
+        trap = GetComponent<ParticleSystem>();
+    }
+}

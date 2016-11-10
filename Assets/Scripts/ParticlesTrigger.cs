94900f2f123f2900a17a85e5d134b9cd243c2d0e
PhoenixFire13 <gaoj@bxscience.edu>
2016-11-10 18:26:57 -0500

    Traps And Collectables Update
    CollectObjects script should be attached to collectable objects (make sure there is a collider). ParticlesTrigger script should be attached to the sphere child of the each trap.

---
+++ Assets/Scripts/ParticlesTrigger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticlesTrigger : MonoBehaviour {
+
+    ParticleSystem trap;
+
+    // Use this for initialization
+    void Awake () {
+        trap = GetComponentInParent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            trap.Play();
+
+            if (tag == "RedTrap")
+            {
+                other.gameObject.GetComponentInChildren<HeartRateManager>().increaseHR(40f);
+            }
+            else if (tag == "BlueTrap")
+            {
+                other.gameObject.GetComponentInChildren<HeartRateManager>().increaseHR(30f);
+            }
+            else if (tag == "GreenTrap")
+            {
+                other.gameObject.GetComponentInChildren<HeartRateManager>().increaseHR(20f);
+            }
+            else if (tag == "YellowTrap")
+            {
+                other.gameObject.GetComponentInChildren<HeartRateManager>().increaseHR(10f);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}

f190fccd4516f52f0f512c9d40598b4f8cb8289e
PhoenixFire13 <gaoj@bxscience.edu>
2016-11-10 20:15:15 -0500

    New Script
    Attach to Canvas. Constantly updates for game status. If game over, play game over animation.

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStatus : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (GameManager.gameOver)
+        {
+            anim.SetTrigger("GameOver");
+        }
+	}
+}
